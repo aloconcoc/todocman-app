4fbff0cfda71de20cd7ff47c1b93bb359a2dff50
leopan <tu416164@gmail.com>
2024-06-26 22:10:13 +0700

    perf

--- app/(drawer)/(tabs)/new-contract/send-mail.tsx
+++ app/(drawer)/(tabs)/new-contract/send-mail.tsx
@@ -132,6 +132,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     fontSize: 12,
     marginRight: 10,
+    maxHeight: 50,
   },
   textContent: {
     textAlignVertical: "top",
