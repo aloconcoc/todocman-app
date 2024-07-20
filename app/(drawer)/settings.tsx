7c27f0fd194517b5ae0e3b154043d56aeaf7b71b
leopan <tu416164@gmail.com>
2024-07-20 16:06:58 +0700

    perf

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -5,84 +5,84 @@ import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import { router } from "expo-router";
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-});
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: true,
+//     shouldSetBadge: true,
+//   }),
+// });
 
-async function sendPushNotification(expoPushToken: string) {
-  const message = {
-    to: expoPushToken,
-    sound: "default",
-    title: "Original Title",
-    body: "And here is the body!",
-    data: { someData: "goes here" },
-  };
+// async function sendPushNotification(expoPushToken: string) {
+//   const message = {
+//     to: expoPushToken,
+//     sound: "default",
+//     title: "Original Title",
+//     body: "And here is the body!",
+//     data: { someData: "goes here" },
+//   };
 
-  await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Accept-encoding": "gzip, deflate",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  });
-}
+//   await fetch("https://exp.host/--/api/v2/push/send", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Accept-encoding": "gzip, deflate",
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(message),
+//   });
+// }
 
-function handleRegistrationError(errorMessage: string) {
-  alert(errorMessage);
-  throw new Error(errorMessage);
-}
+// function handleRegistrationError(errorMessage: string) {
+//   alert(errorMessage);
+//   throw new Error(errorMessage);
+// }
 
-async function registerForPushNotificationsAsync() {
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
+// async function registerForPushNotificationsAsync() {
+//   if (Platform.OS === "android") {
+//     Notifications.setNotificationChannelAsync("default", {
+//       name: "default",
+//       importance: Notifications.AndroidImportance.MAX,
+//       vibrationPattern: [0, 250, 250, 250],
+//       lightColor: "#FF231F7C",
+//     });
+//   }
 
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      handleRegistrationError(
-        "Permission not granted to get push token for push notification!"
-      );
-      return;
-    }
-    const projectId =
-      Constants?.expoConfig?.extra?.eas?.projectId ??
-      Constants?.easConfig?.projectId;
-    if (!projectId) {
-      handleRegistrationError("Project ID not found");
-    }
-    try {
-      const pushTokenString = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId,
-        })
-      ).data;
-      console.log(pushTokenString);
-      return pushTokenString;
-    } catch (e: unknown) {
-      handleRegistrationError(`${e}`);
-    }
-  } else {
-    handleRegistrationError("Must use physical device for push notifications");
-  }
-}
+//   if (Device.isDevice) {
+//     const { status: existingStatus } =
+//       await Notifications.getPermissionsAsync();
+//     let finalStatus = existingStatus;
+//     if (existingStatus !== "granted") {
+//       const { status } = await Notifications.requestPermissionsAsync();
+//       finalStatus = status;
+//     }
+//     if (finalStatus !== "granted") {
+//       handleRegistrationError(
+//         "Permission not granted to get push token for push notification!"
+//       );
+//       return;
+//     }
+//     const projectId =
+//       Constants?.expoConfig?.extra?.eas?.projectId ??
+//       Constants?.easConfig?.projectId;
+//     if (!projectId) {
+//       handleRegistrationError("Project ID not found");
+//     }
+//     try {
+//       const pushTokenString = (
+//         await Notifications.getExpoPushTokenAsync({
+//           projectId,
+//         })
+//       ).data;
+//       console.log(pushTokenString);
+//       return pushTokenString;
+//     } catch (e: unknown) {
+//       handleRegistrationError(`${e}`);
+//     }
+//   } else {
+//     handleRegistrationError("Must use physical device for push notifications");
+//   }
+// }
 
 export default function App() {
   const [expoPushToken, setExpoPushToken] = useState("");
@@ -92,48 +92,48 @@ export default function App() {
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ""))
-      .catch((error: any) => setExpoPushToken(`${error}`));
+  // useEffect(() => {
+  //   registerForPushNotificationsAsync()
+  //     .then((token) => setExpoPushToken(token ?? ""))
+  //     .catch((error: any) => setExpoPushToken(`${error}`));
 
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
+  //   notificationListener.current =
+  //     Notifications.addNotificationReceivedListener((notification) => {
+  //       setNotification(notification);
+  //     });
 
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
+  //   responseListener.current =
+  //     Notifications.addNotificationResponseReceivedListener((response) => {
+  //       console.log(response);
+  //     });
 
-    return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        );
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+  //   return () => {
+  //     notificationListener.current &&
+  //       Notifications.removeNotificationSubscription(
+  //         notificationListener.current
+  //       );
+  //     responseListener.current &&
+  //       Notifications.removeNotificationSubscription(responseListener.current);
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    // Check if there's a notification that triggered the app launch
-    Notifications.getLastNotificationResponseAsync().then((response) => {
-      if (response) {
-        const dataString = response.notification.request.content.dataString;
-        console.log("quit: ", response.notification.request.content);
+  // useEffect(() => {
+  //   // Check if there's a notification that triggered the app launch
+  //   Notifications.getLastNotificationResponseAsync().then((response) => {
+  //     if (response) {
+  //       const dataString = response.notification.request.content.dataString;
+  //       console.log("quit: ", response.notification.request.content);
 
-        if (dataString) {
-          const data = JSON.parse(dataString);
-          const screen = data.screen;
-          if (screen) {
-            router.push(screen);
-          }
-        }
-      }
-    });
-  }, []);
+  //       if (dataString) {
+  //         const data = JSON.parse(dataString);
+  //         const screen = data.screen;
+  //         if (screen) {
+  //           router.push(screen);
+  //         }
+  //       }
+  //     }
+  //   });
+  // }, []);
 
   return (
     <View
@@ -153,7 +153,7 @@ export default function App() {
       <Button
         title="Press to Send Notification"
         onPress={async () => {
-          await sendPushNotification(expoPushToken);
+          // await sendPushNotification(expoPushToken);
         }}
       />
     </View>
