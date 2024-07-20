ba29a4d29025014d0d392be38749a6a222107db9
leopan <tu416164@gmail.com>
2024-07-20 16:08:23 +0700

    useNotification

--- config/tokenUser.ts
+++ config/tokenUser.ts
@@ -82,3 +82,10 @@ export const removeUser = async () => {
     console.log(e);
   }
 };
+export const clearAll = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (e) {
+    console.log(e);
+  }
+};
---
+++ utils/useNotification.ts
@@ -0,0 +1,101 @@
+import { useState, useEffect, useRef } from "react";
+import * as Notifications from "expo-notifications";
+import { router } from "expo-router";
+// import SockJS from "sockjs-client";
+// import { Stomp } from "@stomp/stompjs";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export async function sendPushNotification(expoPushToken: string) {
+  console.log("123", expoPushToken);
+
+  const noti = [];
+  noti.push(expoPushToken);
+  const message = {
+    to: "ExponentPushToken[JHYq3FF8-w7VHNgEqNpbfQ]",
+    sound: "default",
+    title: "title",
+    body: "body",
+    data: { screen: "/(tabs)/explore" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+
+  // await fetch("http://192.168.1.31:2002/send-notification", {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Accept-encoding": "gzip, deflate",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(message),
+  // });
+}
+
+export default function NotificationProvider({ children }: any) {
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("forground: ", notification.request.content.data);
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("background: ", response.notification.request.content.data);
+        const screen = response.notification.request.content.data.screen;
+        if (screen) {
+          router.push(screen);
+        }
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Check if there's a notification that triggered the app launch
+    Notifications.getLastNotificationResponseAsync().then((response) => {
+      if (response) {
+        const dataString = response.notification.request.content.dataString;
+        console.log("quit: ", response.notification.request.content);
+
+        if (dataString) {
+          const data = JSON.parse(dataString);
+          const screen = data.screen;
+          if (screen) {
+            router.push(screen);
+          }
+        }
+      }
+    });
+  }, []);
+
+  return children;
+}
