87264a84354bf50763bcea5964725b2579ce41ff
leopan <tu416164@gmail.com>
2024-07-21 23:46:15 +0700

    update noti screen

--- app/(drawer)/(tabs)/notify/index.tsx
+++ app/(drawer)/(tabs)/notify/index.tsx
@@ -1,4 +1,6 @@
+import { useNotification } from "@/app/Context/NotifyContext";
 import { Entypo, MaterialIcons, Octicons } from "@expo/vector-icons";
+import LottieView from "lottie-react-native";
 import React from "react";
 import {
   View,
@@ -9,135 +11,130 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-const friends = [
-  {
-    id: "1",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "2",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 4,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "3",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "4",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "5",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "6",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "7",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "8",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "9",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-  {
-    id: "10",
-    name: "Duyệt hợp đồng",
-    country: "Hợp đồng buôn lậu",
-    age: "2 năm",
-    rating: 3.5,
-    sports: ["Football", "Baseball", "Basketball", "Soccer"],
-    imageUrl: "https://i.ibb.co/ZXkVtJD/logo-noti.png",
-  },
-];
+const NotifyScreen = () => {
+  const {
+    notifications,
+    totalNotRead,
+    isReadNotify,
+    isDeleteNotify,
+    viewMoreNotify,
+    setNotifications,
+    setTotalNotRead,
+    loading,
+    page,
+    totalPages,
+  } = useNotification();
+  console.log(
+    "bnk: ",
+    notifications,
+    totalNotRead,
+    isReadNotify,
+    isDeleteNotify,
+    viewMoreNotify,
+    setNotifications,
+    setTotalNotRead,
+    loading,
+    page,
+    totalPages
+  );
 
-const FriendListScreen = () => {
+  const handleReadNotify = (id: any, markRead: boolean) => {
+    if (!markRead) {
+      setTotalNotRead((totalNotRead: any) => totalNotRead - 1);
+      setNotifications(
+        notifications?.map((n) => {
+          if (n.id == id) return { ...n, markRead: true };
+          else return n;
+        })
+      );
+      isReadNotify(id);
+    }
+  };
+
+  const handleDeleteNotify = (id: any) => {
+    isDeleteNotify(id);
+  };
+
   const renderFriend = ({ item }: any) => {
     return (
-      <View style={styles.card}>
-        <Image source={{ uri: item.imageUrl }} style={styles.image} />
-        <View style={styles.info}>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.name}>{item.name} </Text>
-            <Octicons name="dot-fill" size={24} color="green" />
+      <TouchableOpacity
+        onPress={() => handleReadNotify(item.id, item.markRead)}
+      >
+        <View style={styles.card}>
+          <Image
+            source={{ uri: "https://i.ibb.co/ZXkVtJD/logo-noti.png" }}
+            style={styles.image}
+          />
+          <View style={styles.info}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Text style={styles.name}>{item.title}</Text>
+              {!item.markRead && (
+                <Octicons name="dot-fill" size={24} color="green" />
+              )}
+            </View>
+            <Text style={styles.details}>{item.message}</Text>
           </View>
-          <Text style={styles.details}>
-            {item.country} - {item.age}
-          </Text>
-          {/* <View style={styles.ratingContainer}>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <Text key={index} style={styles.star}>
-                {index < Math.floor(item.rating) ? "★" : "☆"}
-              </Text>
-            ))}
-          </View> */}
-          {/* <Text style={styles.sports}>{item.sports.join(" | ")}</Text> */}
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleDeleteNotify(item.id)}
+          >
+            <MaterialIcons name="delete" size={24} color="red" />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.button}>
-          <MaterialIcons name="delete" size={24} color="red" />
-          {/* <Text style={styles.buttonText}>Xoá</Text> */}
-        </TouchableOpacity>
-      </View>
+      </TouchableOpacity>
     );
   };
 
+  if (loading) {
+    return (
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
+        }}
+      >
+        <LottieView
+          autoPlay
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "white",
+          }}
+          source={require("@/assets/load.json")}
+        />
+      </View>
+    );
+  }
+
+  if (notifications.length === 0) {
+    return (
+      <View style={styles.emptyContainer}>
+        <Text>Không có thông báo</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
-      data={friends}
+      data={notifications}
       renderItem={renderFriend}
       keyExtractor={(item) => item.id}
       contentContainerStyle={styles.container}
+      ListFooterComponent={() =>
+        page.current + 1 !== totalPages.current && (
+          <View style={styles.footer}>
+            <TouchableOpacity onPress={viewMoreNotify}>
+              <Text style={styles.viewMoreText}>Xem thêm</Text>
+            </TouchableOpacity>
+          </View>
+        )
+      }
     />
   );
 };
@@ -189,9 +186,6 @@ const styles = StyleSheet.create({
   button: {
     flexDirection: "row",
     justifyContent: "center",
-    // backgroundColor: "#fff",
-    // borderWidth: 1,
-    // borderColor: "red",
     paddingVertical: 5,
     paddingHorizontal: 20,
     borderRadius: 5,
@@ -202,6 +196,22 @@ const styles = StyleSheet.create({
     color: "red",
     fontSize: 16,
   },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    borderTopWidth: 2,
+    borderTopColor: "#000",
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  viewMoreText: {
+    color: "blue",
+    textDecorationLine: "underline",
+    cursor: "pointer",
+  },
 });
 
-export default FriendListScreen;
+export default NotifyScreen;
