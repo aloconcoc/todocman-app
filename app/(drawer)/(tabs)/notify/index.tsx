366c24d20dbe11a2859817024be900286009266d
leopan <tu416164@gmail.com>
2024-08-01 17:20:48 +0700

    perf

--- app/(drawer)/(tabs)/notify/index.tsx
+++ app/(drawer)/(tabs)/notify/index.tsx
@@ -93,11 +93,6 @@ const NotifyScreen = () => {
     return (
       <View
         style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
           alignItems: "center",
           justifyContent: "center",
           flex: 1,
@@ -106,8 +101,8 @@ const NotifyScreen = () => {
         <LottieView
           autoPlay
           style={{
-            width: "100%",
-            height: "100%",
+            width: "80%",
+            height: "80%",
             backgroundColor: "white",
           }}
           source={require("@/assets/load.json")}
