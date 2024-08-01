b5b99699c3015d2ea8dedaa00fd4f7338279fead
leopan <tu416164@gmail.com>
2024-08-01 17:20:40 +0700

    perf

--- app/(drawer)/(tabs)/new-contract/send-mail.tsx
+++ app/(drawer)/(tabs)/new-contract/send-mail.tsx
@@ -153,8 +153,8 @@ const SendMail = () => {
         <LottieView
           autoPlay
           style={{
-            width: "100%",
-            height: "100%",
+            width: "80%",
+            height: "80%",
             backgroundColor: "white",
           }}
           source={require("@/assets/load.json")}
@@ -289,11 +289,6 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   loader: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
     flex: 1,
