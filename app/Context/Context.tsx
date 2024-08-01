c4a5946fd19832226ab9c7e7fb0eeddfa8e78ce2
leopan <tu416164@gmail.com>
2024-08-01 17:22:10 +0700

    perf

--- app/Context/Context.tsx
+++ app/Context/Context.tsx
@@ -28,25 +28,18 @@ export const AppProvider = ({ children }: any) => {
     return (
       <View
         style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
           alignItems: "center",
           justifyContent: "center",
           flex: 1,
         }}
       >
-        {/* <Image source={require('../../assets/images/load.jpg')} /> */}
         <LottieView
           autoPlay
           style={{
-            width: "100%",
-            height: "100%",
+            width: "80%",
+            height: "80%",
             backgroundColor: "white",
           }}
-          // Find more Lottie files at https://lottiefiles.com/featured
           source={require("@/assets/load.json")}
         />
       </View>
