5e6ead00d2d8aa385a32f393c906299a88d6ade5
leopan <tu416164@gmail.com>
2024-08-01 17:20:29 +0700

    perf

--- app/(drawer)/(tabs)/new-contract/index.tsx
+++ app/(drawer)/(tabs)/new-contract/index.tsx
@@ -211,8 +211,8 @@ const NewContract = () => {
         <LottieView
           autoPlay
           style={{
-            width: "100%",
-            height: "100%",
+            width: "80%",
+            height: "80%",
             backgroundColor: "white",
           }}
           source={require("@/assets/load.json")}
@@ -895,11 +895,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   loader: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
     flex: 1,
