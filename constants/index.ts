11b193ae341320c4556185940602badd368ebdab
leopan <tu416164@gmail.com>
2024-09-03 21:31:16 +0700

    completed

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,5 @@
-// export const BASE_URL = "http://192.168.1.37:8080/";
-// export const OCR_URL = "http://192.168.1.3:2002/ocr";
-export const OCR_URL = "http://54.242.247.184:2002/ocr";
-export const BASE_URL = "http://54.242.247.184:8080/";
-export const BASE_URL_FE = "https://tdocman.id.vn/";
+export const BASE_URL = "http://192.168.1.37:8080/";
+export const OCR_URL = "http://192.168.1.3:2002/ocr";
 
 export const INTERNAL_SERVER_ERROR = 500;
 export const NOT_FOUND = 404;
